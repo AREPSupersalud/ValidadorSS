b24dc2544d638dd7c35f6718bc4a69de0084b6e0
CarlosCL98 <camer.cl98@gmail.com>
2019-12-09 14:01:51 -0500

    Iniciando validador.

---
+++ src/main/java/edu/eci/arep/ValidadorSS/ValidadorSsApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arep.ValidadorSS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidadorSsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidadorSsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/arep/ValidadorSS/ValidadorSsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arep.ValidadorSS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidadorSsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
